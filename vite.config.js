81bce0335b26a1d5eb8a3bbda58bc7e0593b999b
Darshan Trivedi <trivedidarshan14@gmail.com>
2024-05-12 18:07:08 +0530

    Fix: Vercel ENV fixes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": import.meta.env,
+    VITE_API_KEY: import.meta.env.VITE_API_KEY
+  }
 })
